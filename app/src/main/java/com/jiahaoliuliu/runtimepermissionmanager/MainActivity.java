fb205c82a479c3991c9f76c54c2a66d421b6d7be
Jiahao Liu <jiahao.liu@dubizzle.com>
2016-06-01 12:25:40 +0400

    Trying to request multiple permissions. It is not working

--- app/src/main/java/com/jiahaoliuliu/runtimepermissionmanager/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/runtimepermissionmanager/MainActivity.java
@@ -18,6 +18,7 @@ import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
 import android.widget.Toast;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Simple runtime permission tutorial based on this article
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Contacts";
 
     private final int REQUEST_CODE_ASK_PERMISSIONS = 123;
+    private final int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,29 +39,96 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void insertDummyContactWrapper() {
-        int hasWriteContactsPermission = ContextCompat.checkSelfPermission(MainActivity.this,
-            Manifest.permission.WRITE_CONTACTS);
-        if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
-            if (!ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_CONTACTS)) {
-                showMessageOkCancel("You need to allow access to Contacts",
-                    new DialogInterface.OnClickListener(){
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions(MainActivity.this,
-                                new String[] {Manifest.permission.WRITE_CONTACTS},
-                                REQUEST_CODE_ASK_PERMISSIONS);
-                            return;
-                        }
-                    }
-                );
-                return;
-            }
-            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.WRITE_CONTACTS},
-                REQUEST_CODE_ASK_PERMISSIONS);
-            return;
+        List<String> permissionsNeeded = new ArrayList<String>();
+
+        final List<String> permissionsList = new ArrayList<String>();
+        if (!addPermission(permissionsList, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissionsNeeded.add("GPS");
         }
 
+        if (!addPermission(permissionsList, Manifest.permission.READ_CONTACTS)) {
+            permissionsNeeded.add("Read Contacts");
+        }
+
+        if (!addPermission(permissionsList, Manifest.permission.WRITE_CONTACTS)) {
+            permissionsNeeded.add("Write Contacts");
+        }
+
+        if (permissionsList.size() > 0) {
+            if (permissionsNeeded.size() > 0) {
+                // Needed Rationale
+                String message = "You need to grant access to " + permissionsNeeded.get(0);
+                for (int i = 1; i < permissionsNeeded.size(); i++) {
+                    message = message + ", " + permissionsNeeded.get(i);
+                }
+                showMessageOkCancel(message, new DialogInterface.OnClickListener(){
+                    @Override public void onClick(DialogInterface dialogInterface, int i) {
+                        String[] permissionsArray = permissionsList.toArray(new String[permissionsList.size()]);
+
+                        ActivityCompat.requestPermissions(MainActivity.this, permissionsArray,
+                            REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+                    }
+                });
+                return;
+            }
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                permissionsList.toArray(new String[permissionsList.size()]), REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+
+            return;
+        }
         insertDummyContact();
+    }
+
+    private boolean addPermission(List<String> permissionsList, String permission) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(permission);
+
+            // Check for Rationale Option
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, permission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+        @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS:
+                for (int i = 0; i < permissions.length; i++) {
+                    String permission = permissions[i];
+                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                        Log.v(TAG, permission + "  granted");
+                        Toast.makeText(MainActivity.this, permission + " granted", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Log.v(TAG, permission + " not granted");
+                        Toast.makeText(MainActivity.this, permission + " not granted", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                //if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //    // Permission granted
+                //    insertDummyContact();
+                //} else {
+                //    // Permission denied
+                //    Toast.makeText(MainActivity.this, "WRITE_CONTACTS Denied", Toast.LENGTH_LONG).show();
+                //}
+
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void showMessageOkCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+            .setMessage(message)
+            .setPositiveButton("OK", okListener)
+            .setNegativeButton("Cancel", null)
+            .create()
+            .show();
     }
 
     private void insertDummyContact() {
@@ -93,32 +162,5 @@ public class MainActivity extends AppCompatActivity {
         } catch (OperationApplicationException e) {
             Log.d(TAG, "Could not add a new contact: " + e.getMessage());
         }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-        @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CODE_ASK_PERMISSIONS:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Permission granted
-                    insertDummyContact();
-                } else {
-                    // Permission denied
-                    Toast.makeText(MainActivity.this, "WRITE_CONTACTS Denied", Toast.LENGTH_LONG).show();
-                }
-                break;
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
-
-    private void showMessageOkCancel(String message, DialogInterface.OnClickListener okListener) {
-        new AlertDialog.Builder(MainActivity.this)
-            .setMessage(message)
-            .setPositiveButton("OK", okListener)
-            .setNegativeButton("Cancel", null)
-            .create()
-            .show();
     }
 }
