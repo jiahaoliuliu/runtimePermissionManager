cc4960e06b56f75fb621e3759e28ab5c6bfc0fb7
Jiahao Liu <jiahao.liu@dubizzle.com>
2016-06-01 11:55:23 +0400

    Created simple app that handles write contact permission

--- app/src/main/java/com/jiahaoliuliu/runtimepermissionmanager/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/runtimepermissionmanager/MainActivity.java
@@ -1,12 +1,124 @@
 package com.jiahaoliuliu.runtimepermissionmanager;
 
+import android.Manifest;
+import android.content.ContentProviderOperation;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.OperationApplicationException;
+import android.content.pm.PackageManager;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.util.Log;
+import android.widget.Toast;
+import java.util.ArrayList;
 
+/**
+ * Simple runtime permission tutorial based on this article
+ * https://inthecheesefactory.com/blog/things-you-need-to-know-about-android-m-permission-developer-edition/en
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Contacts";
+
+    private final int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        insertDummyContactWrapper();
+    }
+
+    private void insertDummyContactWrapper() {
+        int hasWriteContactsPermission = ContextCompat.checkSelfPermission(MainActivity.this,
+            Manifest.permission.WRITE_CONTACTS);
+        if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_CONTACTS)) {
+                showMessageOkCancel("You need to allow access to Contacts",
+                    new DialogInterface.OnClickListener(){
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[] {Manifest.permission.WRITE_CONTACTS},
+                                REQUEST_CODE_ASK_PERMISSIONS);
+                            return;
+                        }
+                    }
+                );
+                return;
+            }
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.WRITE_CONTACTS},
+                REQUEST_CODE_ASK_PERMISSIONS);
+            return;
+        }
+
+        insertDummyContact();
+    }
+
+    private void insertDummyContact() {
+        // Two operations are needed to insert a new contact
+        ArrayList<ContentProviderOperation> operations = new ArrayList<ContentProviderOperation>();
+
+        // First, set up a new raw contact
+        ContentProviderOperation.Builder op =
+            ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null);
+
+        operations.add(op.build());
+
+        // Next, set the name for the contact.
+
+        op = ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+            .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
+            .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+            .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,
+                "__DUMMY CONTACT from runtime permissions sample");
+
+        operations.add(op.build());
+
+        // Apply the operations.
+        ContentResolver resolver = getContentResolver();
+        try {
+            resolver.applyBatch(ContactsContract.AUTHORITY, operations);
+        } catch (RemoteException e) {
+            Log.d(TAG, "Could not add a new contact: " + e.getMessage());
+        } catch (OperationApplicationException e) {
+            Log.d(TAG, "Could not add a new contact: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+        @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                    insertDummyContact();
+                } else {
+                    // Permission denied
+                    Toast.makeText(MainActivity.this, "WRITE_CONTACTS Denied", Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void showMessageOkCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+            .setMessage(message)
+            .setPositiveButton("OK", okListener)
+            .setNegativeButton("Cancel", null)
+            .create()
+            .show();
     }
 }
